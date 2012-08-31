c70d68b1a1cc1721e2bc0a0751bfda24e778fa81
杨振海 <yzh0709@gmail.com>
2012-08-31 15:37:30 +0800

    提交搜索结果及搜索返回方法

--- app.js
+++ app.js
@@ -33,3 +33,22 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+var Db = require('mongodb').Db;
+var Server = require('mongodb').Server;
+
+app.get('/showUsers',function(req,res){
+    var users=[];
+
+    var db=new Db('test',new Server('localhost',27017,{auto_reconnect:true}, {}));
+    db.open(function(){
+        db.collection('my_users',function(err,collection){
+            if (err) callback(err);
+            collection.find({}).toArray(function(err,docs){
+                if (err) callback(err);
+
+                res.render('index', { items:docs,title:'搜索结果' });
+            });
+        });
+    });
+    res.end();
+});
